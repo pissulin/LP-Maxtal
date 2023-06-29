eb28803a21b728bac17c7c9fdd6ac7bde90c372d
Anderson Pissulin <pissulin.ads@gmail.com>
2023-06-29 10:12:04 -0300

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    $to = "a.pissulin@gmail.com";
+    $subject = "Novo formulário de contato";
+    $emailBody = "Nome: $name\n";
+    $emailBody .= "Email: $email\n";
+    $emailBody .= "Mensagem: $message\n";
+
+    if (mail($to, $subject, $emailBody)) {
+        http_response_code(200);
+    } else {
+        http_response_code(500);
+    }
+} else {
+    http_response_code(400);
+}
+?>
---
+++ script.js
@@ -0,0 +1,29 @@
+// script.js
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    var formData = new FormData();
+    formData.append("name", name);
+    formData.append("email", email);
+    formData.append("message", message);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "enviar.php", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            console.log("Formulário enviado com sucesso!");
+            // Aqui você pode adicionar a lógica adicional, como exibir uma mensagem de sucesso ou redirecionar para outra página
+        } else {
+            console.log("Erro ao enviar o formulário.");
+            // Aqui você pode adicionar a lógica adicional, como exibir uma mensagem de erro ao usuário
+        }
+    };
+
+    xhr.send(formData);
+});
